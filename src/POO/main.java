e690832df50493cd5ca519e7d5baf411ca983727
Bowie <andersoncruz2908@gmail.com>
2023-11-29 22:32:46 -0300

    Novas linhas Add

--- src/POO/main.java
+++ src/POO/main.java
@@ -3,12 +3,12 @@ package POO;
 public class main {
 
   public static void main(String[] args) {
-    System.out.println("--- QUESTÃO 01---");
+    System.out.println(" Está tudo ok ");
     carro meucarro = new carro();
     meucarro.setCarroInfo("Toyota", "Corolla", 2023);
     meucarro.exibirCarroInfo();
 
-    System.out.println("--- QUESTÃO 02---");
+    System.out.println(" Está tudo ok ");
     meucarro.rodar(100);
     meucarro.rodar(-100);
     meucarro.rodar(50);
@@ -17,7 +17,7 @@ public class main {
     meucarro.rodar(-3);
     System.out.println("Quilometragem: "+meucarro.getQuilometratem());
   
-    System.out.println("--- QUESTÃO 03---");
+    System.out.println(" Está tudo ok ");
         CarroEsportivo meuCarroEsportivo = new CarroEsportivo();
         meuCarroEsportivo.setCarroInfo("Ferrari", "488 GTB", 2023);
         meuCarroEsportivo.setVelocidademaxima(330);
@@ -26,7 +26,7 @@ public class main {
         meuCarroEsportivo.setVelocidademaxima(80);
         System.out.println(meuCarroEsportivo);
         
-        System.out.println("--- QUESTÃO 04---");
+        System.out.println(" Está tudo ok ");
         Motor motorCarro = new Motor("V8", 500);
         
         carro meuCarroComMotor = new carro("Chevrolet", "Camaro", 2023, motorCarro);
