0095563aa7d4a079e281baf48e1652ec5d310007
Bowie <andersoncruz2908@gmail.com>
2023-11-29 21:58:34 -0300

    New line Code

--- src/POO/main.java
+++ src/POO/main.java
@@ -3,6 +3,18 @@ package POO;
 public class main {
 
   public static void main(String[] args) {
-    
+    System.out.println("--- QUESTÃO 01---");
+    carro meucarro = new carro();
+    meucarro.setCarroInfo("Toyota", "Corolla", 2023);
+    meucarro.exibirCarroInfo();
+
+    System.out.println("--- QUESTÃO 02---");
+    meucarro.rodar(100);
+    meucarro.rodar(-100);
+    meucarro.rodar(50);
+    meucarro.rodar(73);
+    meucarro.rodar(122);
+    meucarro.rodar(-3);
+    System.out.println("Quilometragem: "+meucarro.getQuilometratem());
   
 }
