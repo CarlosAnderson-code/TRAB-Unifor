8e6d5b3e4f608b1070dbd45036f01ca65d0354dd
Sasousa82 <145471831+Sasousa82@users.noreply.github.com>
2023-11-30 19:17:38 -0300

    Create motor.java
    O código do motor

---
+++ src/POO/motor.java
@@ -0,0 +1,25 @@
+public class Motor {
+    private String tipo;
+    private int potencia;
+
+    public Motor(String tipo, int potencia) {
+        this.tipo = tipo;
+        this.potencia = potencia;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getPotencia() {
+        return potencia;
+    }
+    
+    public void setTipo(int potencia){
+        this.potencia = potencia;
+    }
+}
